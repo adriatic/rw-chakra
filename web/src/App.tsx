2cf4a984da257f064e9d99a83a816dad4b3b6f7f
nik <nik@Nikolajs-MacBook-Pro.local>
2023-02-26 12:24:09 -0500

    update to react 18

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,6 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -6,12 +9,25 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <ColorModeScript />
+      <ChakraProvider theme={theme}>
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </ChakraProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
